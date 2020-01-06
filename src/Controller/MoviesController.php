fb4ecd18812c942adb630ab8d9cbeb8e2befd18f
pierre <nanashi4781@gmail.com>
2020-01-06 15:09:03 +0100

    relation ok

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Movies;
+
+
+class MoviesController extends AbstractController
+{
+    /**
+     * @Route("/movies", name="movies")
+     */
+    public function index()
+    {
+        $movies = $this->getDoctrine()
+        ->getRepository(Movies::class)
+        ->findBy([], ['name'=>'ASC'], 20, 0);
+
+        dump($movies[0]->getCategory()[0]);
+
+        return $this->render('movies/index.html.twig', [
+            'movies' => $movies,
+        ]);
+    }
+}
