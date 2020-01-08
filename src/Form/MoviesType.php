f411fce9a998c63bd40d9ec828178a16e9b960f3
pierre <nanashi4781@gmail.com>
2020-01-08 16:17:54 +0100

    addMovie

--- src/Form/MoviesType.php
+++ src/Form/MoviesType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 use App\Entity\Authors;
@@ -17,15 +18,40 @@ class MoviesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-       ->add('name', TextType::class)
-       ->add('description', TextareaType::class)
-       ->add('date', DateType::class)
-       ->add('country', TextType::class)
-       ->add('cover', TextType::class)
-       ->add('link', TextType::class)
+       ->add('name', TextType::class, [
+           'label' => "Nom du film",
+           'required' => false,
+       ])
+       ->add('description', TextareaType::class,[
+           'required' => false,
+       ])
+       ->add('date', DateType::class, [
+       'years' => range(1950, date('Y')),
+       'label' => "Date de sortie",
+       'required' => false,
+        ])
+       ->add('country', TextType::class,[
+           'label' => "Pays",
+           'required' => false,
+       ])
+       ->add('cover', TextType::class, [
+           'label' => "Affiche",
+           'required' => false,
+       ])
+       ->add('link', TextType::class, [
+           'label' => "Lien Allociné",
+           'required' => false,
+       ])
        ->add('author', EntityType::class, [
         'class' => Authors::class,
         'choice_label' => 'name',
+        'label' => "Réalisateur",
+        'required' => false,
+        'placeholder' => "Sélectionner un réalisateur",
+        ])
+        ->add('save', SubmitType::class,[
+            'label' => "Valider",
         ]);
+
     }
 }
