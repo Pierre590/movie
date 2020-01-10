c24366deca19da1adbbd125ad16c08dc0c910acd
pierre <nanashi4781@gmail.com>
2020-01-10 14:06:44 +0100

    infinitescroll

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -10,32 +10,41 @@ use Knp\Component\Pager\PaginatorInterface;
 
 class MoviesController extends AbstractController
 {
+    const LIMIT = 21;
 
     /**
      * @Route("/movies", name="movies")
      */
-    public function index(Request $request, PaginatorInterface $paginator)
+    public function index(Request $request)
+    {
+        return $this->render('movies/index.html.twig', [
+            'limit' => self::LIMIT,
+        ]);
+    }
+
+    /**
+     * @Route("/movies/ajax", name="movies_ajax")
+     */
+    public function moviesAjax(Request $request)
     {
         $search = (string) $request->query->get('search', null);
+        $offset = (int) $request->query->get('offset', 0);
 
         $movies = $this->getDoctrine()
         ->getRepository(Movies::class)
         ->createQueryBuilder('m')
         ->where('m.name LIKE :name')
         ->setParameter('name', '%' . $search .'%')
+        ->setMaxResults(self::LIMIT)
+        ->setFirstResult($offset)
         ->orderBy('m.name')
         ->getQuery()
         ->execute();
 
-        $list = $paginator->paginate(
-            $movies,
-            $request->query->getInt('page', 1),
-            12
-        );
-
-        return $this->render('movies/index.html.twig', [
-            'list' => $list,
+        return $this->render('movies/ajax.html.twig', [
+            'movies' => $movies,
         ]);
+
     }
 
     /**
@@ -52,7 +61,6 @@ class MoviesController extends AbstractController
             'movie' => $movie,
         ]);
     }
-
 
 
 }
